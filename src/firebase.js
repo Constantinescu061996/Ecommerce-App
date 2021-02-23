7ba3dc3030655179e67e395a1cb8f425de58b493
Catalin <catalin.constantinescu06@yahoo.com>
2021-02-23 21:11:25 +0200

    Nav and Home

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC6yTjRt2l65pQEgHVazATso0V8wBoaHdM",
+    authDomain: "clone-romania.firebaseapp.com",
+    projectId: "clone-romania",
+    storageBucket: "clone-romania.appspot.com",
+    messagingSenderId: "789009498740",
+    appId: "1:789009498740:web:f14e7f66063b5addf5c721",
+    measurementId: "G-0ZZ5H12WJG"
+  };
